055f27c079487bd5c40d823cd2a4b14860a81752
SYA <SYA@SYA-PC1>
2023-05-11 17:09:18 +0900

    05/11-3

--- src/main/java/com/study/DepartmentRepository.java
+++ src/main/java/com/study/DepartmentRepository.java
@@ -3,4 +3,4 @@ package com.study;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface DepartmentRepository extends JpaRepository<Department, Long> { // @Repositoryを書かないサンプルも多い
-}
+}//
--- src/main/java/com/study/EmployeeRepository.java
+++ src/main/java/com/study/EmployeeRepository.java
@@ -3,4 +3,4 @@ package com.study;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface EmployeeRepository extends JpaRepository<Employee, Long> { // @Repositoryを書かないサンプルも多い
-}
+}//
